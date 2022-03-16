4d1845f779ee20866cf39a71f46cee44d2c522f0
Manuel <manueljsandalio@gmail.com>
2022-03-16 12:43:39 +0100

    Fin sección 10 - Auth

--- database/config.js
+++ database/config.js
@@ -1,37 +1,25 @@
 const mongoose = require('mongoose');
 
-const dbConnection = async () => {
 
-    //DEPRECADO:
+const dbConnection = async() => {
 
-    // try {
-    //     await mongoose.connect('mongodb+srv://mean_user:BB3Abf3SnVjqQegi@cluster0.o4bqi.mongodb.net/hospitaldb', {
-    //         useNewUrlParser: true,
-    //         useUnifiedTopology: true,
-    //         useCreateIndex: true
-    //     });
+    try {
+        await mongoose.connect( process.env.DB_CNN , {
+            useNewUrlParser: true, 
+            useUnifiedTopology: true,
+            useCreateIndex: true
+        });
 
-    //     console.log('DB Online');
-
-    // } catch (error) {
-    //     console.log(error);
-    //     throw new Error ('Error a la hora de iniciar la DB ver Logs');
-    // }
-
-    //NUEVA VERSION:
-    const dbConnection = async () => {
-        try {
-            await mongoose.connect(process.env.DB_CNN, {
-                
-            });
-            console.log('Connection succsessful');
-        } catch (error) {
-            console.error(error);
-            throw new Error('Error trying to connect');
-        }
+        console.log('DB Online');
+        
+    } catch (error) {
+        console.log(error);
+        throw new Error('Error a la hora de iniciar la BD ver logs');
     }
 
+
 }
+
 
 module.exports = {
     dbConnection
--- index.js
+++ index.js
@@ -1,28 +1,29 @@
 require('dotenv').config();
+
 const express = require('express');
-const cors = require('cors')
+const cors = require('cors');
 
 const { dbConnection } = require('./database/config');
 
-// Crear el servidores de express
+// Crear el servidor de express
 const app = express();
 
-//Configurar CORS
-app.use(cors());
+// Configurar CORS
+app.use( cors() );
+
+// Lectura y parseo del body
+app.use( express.json() );
 
 // Base de datos
 dbConnection();
 
+
 // Rutas
-app.get( '/', (req, res) => {
+app.use( '/api/usuarios', require('./routes/usuarios') );
+app.use( '/api/login', require('./routes/auth') );
 
-    res.json({
-        ok: true,
-        msg: 'Hola Mundo'
-    })
-
-});
 
 app.listen( process.env.PORT, () => {
-    console.log ('Servidor corriendo en puero ' + process.env.PORT);
+    console.log('Servidor corriendo en puerto ' + process.env.PORT );
 });
+
